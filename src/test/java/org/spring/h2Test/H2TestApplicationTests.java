4439baf0f91a6bdf7c604da811778962d9472591
unknown <egonzalezsuarez07@gmail.com>
2023-02-15 00:03:25 -0500

    conf h2

---
+++ src/test/java/org/spring/h2Test/H2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.h2Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
